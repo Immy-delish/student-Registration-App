7899470d9f703577878e52d4199816a355679ac1
Immaculate Atim <immaculateatim56@gmail.com>
2024-04-03 17:43:03 +0300

    Added A New Student Registration Application

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <style>
+            P{
+                text-align: center;
+            }
+        </style>    
+        <b>
+    <P>These are my mentees for PHP training Classes. <br> I love to see people learn to Code with passion.
+     <br>They have to become great PHP software Developers</P>
+    </b>
+    <br>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Registration Book</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+        </script>
+    </head>
+    <body>
+        <div class="container mt-5">
+            <form method="POST" action="/addcontact">
+                @csrf
+                <div class="form-group mb-2">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" name="email" placeholder="Enter email">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="exampleInputPassword1">Phone Number</label>
+                    <input type="text" class="form-control" name="phone" placeholder="Phone">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="exampleInputPassword1">Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Name">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <table class="table mt-5">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if (count($contact) > 0)
+                        @foreach ($contact as $cont)
+                            <tr>
+                                <th>{{ $cont->id }}</th>
+                                <th>{{ $cont->name }}</th>
+                                <th>{{ $cont->phone }}</th>
+                                <th>{{ $cont->email }}</th>
+                                <th><a href="/edit/{{ $cont->id }}" class="btn btn-primary">Edit</a>
+                                    <a href="/delete/{{ $cont->id }}" class="btn btn-danger">Delete</a>
+                                </th>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <th>No Data</th>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/addcontact', [ContactController::class, 'add']);
+Route::get('/delete/{id}', [ContactController::class, 'delete']);
+Route::get('/edit/{id}', [ContactController::class, 'edit']);
+Route::post('/edit/{id}', [ContactController::class, 'update']);
