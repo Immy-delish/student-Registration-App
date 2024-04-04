f9886c7de001230b6e03a804cff905429c275f40
Immaculate Atim <immaculateatim56@gmail.com>
2024-04-04 16:30:38 +0300

    Displayed the list of Registered students in Accordion.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,12 @@
             integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
         </script>
     </head>
+    <style>
+      p {
+          text-align: center;
+      }
+  </style> 
+    <h4><p><b>Students for the available courses.</b></p></h4>
     <body>
         <div class="container mt-5">
             <form method="POST" action="/addcontact">
@@ -35,36 +41,95 @@
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-            <table class="table mt-5">
-                <thead>
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if (count($contact) > 0)
-                        @foreach ($contact as $cont)
-                            <tr>
-                                <th>{{ $cont->id }}</th>
-                                <th>{{ $cont->name }}</th>
-                                <th>{{ $cont->phone }}</th>
-                                <th>{{ $cont->email }}</th>
-                                <th><a href="/edit/{{ $cont->id }}" class="btn btn-primary">Edit</a>
-                                    <a href="/delete/{{ $cont->id }}" class="btn btn-danger">Delete</a>
-                                </th>
-                            </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <th>No Data</th>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <strong> List of Registered students.</strong>
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <strong>
+                  <table class="table mt-5">
+                      <thead>
+                          <tr>
+                              <th scope="col">Id</th>
+                              <th scope="col">Name</th>
+                              <th scope="col">Phone</th>
+                              <th scope="col">Email</th>
+                              <th scope="col">Action</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          @if (count($contact) > 0)
+                              @foreach ($contact as $cont)
+                                  <tr>
+                                      <th>{{ $cont->id }}</th>
+                                      <th>{{ $cont->name }}</th>
+                                      <th>{{ $cont->phone }}</th>
+                                      <th>{{ $cont->email }}</th>
+                                      <th><a href="/edit/{{ $cont->id }}" class="btn btn-primary">Edit</a>
+                                          <a href="/delete/{{ $cont->id }}" class="btn btn-danger">Delete</a>
+                                      </th>
+                                  </tr>
+                              @endforeach
+                          @else
+                              <tr>
+                                  <th>No Data</th>
+                              </tr>
+                          @endif
+                      </tbody>
+                  </table>
+              <!--</div></strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              </div>-->
+            </div>
+          </div>
+          <!--<div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Accordion Item #2
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              </div>
+            </div>
+          </div> -->
+           <!--<div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Accordion Item #3
+              </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              </div>
+            </div>
+          </div> -->
+        </div>
+        <span class="d-block p-2 text-gray"></span>
+        <div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Students Who Have Fully Registered</h5>
+                <p class="card-text">These students have fully registered and paid Tuition</p>
+                <a href="#" class="btn btn-primary">Assign Lecturers</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Not Fully Registered Students</h5>
+                <p class="card-text">These students have pending balances and missing Documents.</p>
+                <a href="#" class="btn btn-primary">Send Notice</a>
+              </div>
+            </div>
+          </div>
         </div>
     </body>
 </html>
