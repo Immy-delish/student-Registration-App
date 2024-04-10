fff59c71df3e35e0ef1ddafea1ceacca6cdbb51d
Immaculate Atim <immaculateatim56@gmail.com>
2024-04-10 17:44:07 +0300

    Created Register and Login pages.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginRegisterController;
+
+
 
 
 
@@ -34,6 +37,15 @@ Route::get('/', function() {
     return view('home');
  });
 
- Route::get('/devschool/courses', function() {
+Route::get('/devschool/courses', function() {
     return view('courses');
  });
+
+ Route::controller(LoginRegisterController::class)->group(function() {
+   Route::get('/register', 'register')->name('register');
+   Route::post('/store', 'store')->name('store');
+   Route::get('/login', 'login')->name('login');
+   Route::post('/authenticate', 'authenticate')->name('authenticate');
+   Route::get('/dashboard', 'dashboard')->name('dashboard');
+   Route::post('/logout', 'logout')->name('logout');
+});
